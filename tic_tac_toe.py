f2fe2fd3e3f6d9e73d31372bddaf8dc6fc98089c
dani <dan_yeet@protonmail.com>
2024-10-17 23:32:19 +0300

    refactoring print function and pep8 compliance

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -11,6 +11,9 @@ def print_board(board):
     # print()
 
 
+USER_MOVE = "X"
+CPU_MOVE = "O"
+EMPTY_PLACE = ""
 board = [["", "", ""], ["", "", ""], ["", "", ""]]
 
 if __name__ == "__main__":
@@ -30,7 +33,7 @@ if __name__ == "__main__":
                 usr_input_row = (
                     int(input("Please enter a valid row number between 1 and 3:")) - 1
                 )
-            elif board[usr_input_row][usr_input_col] != "":
+            elif board[usr_input_row][usr_input_col] != EMPTY_PLACE:
                 print("Please enter a position that is correct and not already used.")
                 usr_input_col = int(input("Enter column number between 1 and 3:")) - 1
                 usr_input_row = (
@@ -38,41 +41,73 @@ if __name__ == "__main__":
                 )
             else:
                 break
-        board[usr_input_row][usr_input_col] = "X"
+        board[usr_input_row][usr_input_col] = USER_MOVE
         counter_moves += 1
 
         # Column wins
-        if board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
+        if (
+            board[0][0] == USER_MOVE
+            and board[1][0] == USER_MOVE
+            and board[2][0] == USER_MOVE
+        ):
             print("Congratulations, you won!")
             print_board(board)
             break
-        elif board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
+        elif (
+            board[0][1] == USER_MOVE
+            and board[1][1] == USER_MOVE
+            and board[2][1] == USER_MOVE
+        ):
             print("Congratulations, you won!")
             print_board(board)
             break
-        elif board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X":
+        elif (
+            board[0][2] == USER_MOVE
+            and board[1][2] == USER_MOVE
+            and board[2][2] == USER_MOVE
+        ):
             print("Congratulations, you won!")
             print_board(board)
             break
         # Row wins
-        elif board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X":
+        elif (
+            board[0][0] == USER_MOVE
+            and board[0][1] == USER_MOVE
+            and board[0][2] == USER_MOVE
+        ):
             print("Congratulations, you won!")
             print_board(board)
             break
-        elif board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X":
+        elif (
+            board[1][0] == USER_MOVE
+            and board[1][1] == USER_MOVE
+            and board[1][2] == USER_MOVE
+        ):
             print("Congratulations, you won!")
             print_board(board)
             break
-        elif board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X":
+        elif (
+            board[2][0] == USER_MOVE
+            and board[2][1] == USER_MOVE
+            and board[2][2] == USER_MOVE
+        ):
             print("Congratulations, you won!")
             print_board(board)
             break
         # Diagonal wins
-        elif board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
+        elif (
+            board[0][0] == USER_MOVE
+            and board[1][1] == USER_MOVE
+            and board[2][2] == USER_MOVE
+        ):
             print("Congratulations, you won!")
             print_board(board)
             break
-        elif board[2][0] == "X" and board[1][1] == "X" and board[0][2] == "X":
+        elif (
+            board[2][0] == USER_MOVE
+            and board[1][1] == USER_MOVE
+            and board[0][2] == USER_MOVE
+        ):
             print("Congratulations, you won!")
             print_board(board)
             break
@@ -84,44 +119,76 @@ if __name__ == "__main__":
         while 1:
             random_col = random.randint(0, 2)
             random_row = random.randint(0, 2)
-            if board[random_row][random_col] != "":
+            if board[random_row][random_col] != EMPTY_PLACE:
                 random_col = random.randint(0, 2)
                 random_row = random.randint(0, 2)
             else:
-                board[random_row][random_col] = "O"
+                board[random_row][random_col] = CPU_MOVE
                 counter_moves += 1
                 break
-        if board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O":
+        if (
+            board[0][0] == CPU_MOVE
+            and board[1][0] == CPU_MOVE
+            and board[2][0] == CPU_MOVE
+        ):
             print("You lost!")
             print_board(board)
             break
-        elif board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
+        elif (
+            board[0][1] == CPU_MOVE
+            and board[1][1] == CPU_MOVE
+            and board[2][1] == CPU_MOVE
+        ):
             print("You lost!")
             print_board(board)
             break
-        elif board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O":
+        elif (
+            board[0][2] == CPU_MOVE
+            and board[1][2] == CPU_MOVE
+            and board[2][2] == CPU_MOVE
+        ):
             print("You lost!")
             print_board(board)
             break
         # Row wins
-        elif board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O":
+        elif (
+            board[0][0] == CPU_MOVE
+            and board[0][1] == CPU_MOVE
+            and board[0][2] == CPU_MOVE
+        ):
             print("You lost!")
             print_board(board)
             break
-        elif board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O":
+        elif (
+            board[1][0] == CPU_MOVE
+            and board[1][1] == CPU_MOVE
+            and board[1][2] == CPU_MOVE
+        ):
             print("You lost!")
             print_board(board)
             break
-        elif board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O":
+        elif (
+            board[2][0] == CPU_MOVE
+            and board[2][1] == CPU_MOVE
+            and board[2][2] == CPU_MOVE
+        ):
             print("You lost!")
             print_board(board)
             break
         # Diagonal wins
-        elif board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+        elif (
+            board[0][0] == CPU_MOVE
+            and board[1][1] == CPU_MOVE
+            and board[2][2] == CPU_MOVE
+        ):
             print("You lost!")
             print_board(board)
             break
-        elif board[2][0] == "O" and board[1][1] == "O" and board[0][2] == "O":
+        elif (
+            board[2][0] == CPU_MOVE
+            and board[1][1] == CPU_MOVE
+            and board[0][2] == CPU_MOVE
+        ):
             print("You lost!")
             print_board(board)
             break
