8e18c9d8b436932a152637712bc6d97eaf6ecf57
dani <dan_yeet@protonmail.com>
2024-10-17 23:26:22 +0300

    refactoring print function and pep8 compliance

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,14 +1,17 @@
 import random
+
+
 # Define the board and print board method
 def print_board(board):
-    print(board[0][0] + "|"+ board[0][1] + "|"+ board[0][2])
-    print(board[1][0] + "|"+ board[1][1] + "|"+ board[1][2])
-    print(board[2][0] + "|"+ board[2][1] + "|"+ board[2][2])
-    print()
+    for row in board:
+        print(f"{row[0]} | {row[1]} | {row[2]} ")
+    # print(board[0][0] + "|"+ board[0][1] + "|"+ board[0][2])
+    # print(board[1][0] + "|"+ board[1][1] + "|"+ board[1][2])
+    # print(board[2][0] + "|"+ board[2][1] + "|"+ board[2][2])
+    # print()
 
-board = [["","",""],
-         ["","",""],
-         ["","",""]]
+
+board = [["", "", ""], ["", "", ""], ["", "", ""]]
 
 if __name__ == "__main__":
     counter_moves = 0
@@ -19,24 +22,31 @@ if __name__ == "__main__":
         usr_input_row = int(input("Enter row number between 1 and 3:")) - 1
         while 1:
             if usr_input_col > 2 or usr_input_col < 0:
-                usr_input_col = int(input("Please enter a valid column number between 1 and 3:")) - 1
+                usr_input_col = (
+                    int(input("Please enter a valid column number between 1 and 3:"))
+                    - 1
+                )
             elif usr_input_row > 2 or usr_input_row < 0:
-                usr_input_row = int(input("Please enter a valid row number between 1 and 3:")) - 1
+                usr_input_row = (
+                    int(input("Please enter a valid row number between 1 and 3:")) - 1
+                )
             elif board[usr_input_row][usr_input_col] != "":
                 print("Please enter a position that is correct and not already used.")
                 usr_input_col = int(input("Enter column number between 1 and 3:")) - 1
-                usr_input_row = int(input("Please enter a valid row number between 1 and 3:")) - 1
+                usr_input_row = (
+                    int(input("Please enter a valid row number between 1 and 3:")) - 1
+                )
             else:
                 break
         board[usr_input_row][usr_input_col] = "X"
         counter_moves += 1
-        
+
         # Column wins
         if board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
             print("Congratulations, you won!")
             print_board(board)
             break
-        elif board[0][1] == "X" and board[1][1]  == "X" and board[2][1] == "X":
+        elif board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
             print("Congratulations, you won!")
             print_board(board)
             break
@@ -71,12 +81,12 @@ if __name__ == "__main__":
             print("It's a tie")
             print_board(board)
             break
-        while 1:    
-            random_col = random.randint(0,2)
-            random_row = random.randint(0,2)
+        while 1:
+            random_col = random.randint(0, 2)
+            random_row = random.randint(0, 2)
             if board[random_row][random_col] != "":
-                random_col = random.randint(0,2)
-                random_row = random.randint(0,2)
+                random_col = random.randint(0, 2)
+                random_row = random.randint(0, 2)
             else:
                 board[random_row][random_col] = "O"
                 counter_moves += 1
@@ -85,7 +95,7 @@ if __name__ == "__main__":
             print("You lost!")
             print_board(board)
             break
-        elif board[0][1] == "O" and board[1][1]  == "O" and board[2][1] == "O":
+        elif board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
             print("You lost!")
             print_board(board)
             break
